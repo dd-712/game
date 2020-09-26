0a21c32eaa1aec3c0ab13fc1cbe02bf8b5faff83
dd-712 <66867526+dd-712@users.noreply.github.com>
2020-09-26 18:16:33 +0530

    Update snake.js

--- snake.js
+++ snake.js
@@ -9,12 +9,12 @@ let right = new Audio();
 let left = new Audio();
 let down = new Audio();
 
-dead.src = "../audio/dead.mp3";
-eat.src = "../audio/eat.mp3";
-up.src = "../audio/up.mp3";
-right.src = "../audio/right.mp3";
-left.src = "../audio/left.mp3";
-down.src = "../audio/down.mp3";
+dead.src = "audio/dead.mp3";
+eat.src = "audio/eat.mp3";
+up.src = "audio/up.mp3";
+right.src ="audio/right.mp3";
+left.src = "audio/left.mp3";
+down.src = "audio/down.mp3";
 
 var ctx=cvs.getContext("2d");
 var snakeW=25
